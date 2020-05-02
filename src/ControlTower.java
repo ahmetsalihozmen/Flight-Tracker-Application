9c59f3e2e4ab03449b7cd3773f5d5308164041f2
Şafak Bilici <44443472+safakkbilici@users.noreply.github.com>
2020-05-02 21:44:42 +0300

    Create ControlTower.java

---
+++ src/ControlTower.java
@@ -0,0 +1,11 @@
+public class ControlTower {
+	public String towerName;
+	
+	public ControlTower(String towerName) {
+		this.towerName = towerName;
+	}
+	
+	public boolean cancelFlight() {
+		return false;
+	}
+}
