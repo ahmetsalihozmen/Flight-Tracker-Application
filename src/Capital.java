8b9eaea9f9918bb141650fa2d5c50898581961fe
Şafak Bilici <44443472+safakkbilici@users.noreply.github.com>
2020-05-02 21:44:15 +0300

    Create Capital.java

---
+++ src/Capital.java
@@ -0,0 +1,30 @@
+public class Capital {
+	
+	private String country;
+	private String continent;
+	private String capitalName;
+	private ControlTower controltw;
+	
+	public Capital(String capitalName, ControlTower controltw) {
+		this.capitalName = capitalName;
+		this.controltw = controltw;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	public String getCapitalName() {
+		return capitalName;
+	}
+	public String getContinent() {
+		return continent;
+	}
+	public ControlTower getControltw() {
+		return controltw;
+	}
+
+	public boolean signalFromControlTower() {
+		return controltw.cancelFlight();
+	}
+
+}
